b1e11d93bea4b24360066504ad7b448e5cf82017
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-12-21 00:07:14 -0600

    added selectors

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+ $('h1#').hide();
+
+$('h1#heading1').hide();
+
+$('h1.heading2').hide();
+$('.heading2').hide();
+
+$("p span").css("color", "red");
+$("span").css("color", "red");
+
+$('ul#list li:first').css('color', 'red');
+$('ul#list li:last').css('color', 'blue');
+$('ul#list li:even').css('background-color', 'green');
+$('ul#list li:odd').css('background-color', '#ccc');
+
+// bullet removed from every 3rd list item //
+$('ul#list li:nth-child(3n)').css('list-style', 'none');
+
+$(':button').hide();
+$(':submit').hide();
+$(':text').hide();
+
+$('[href]').css('color', 'red');
+$('a[href="http://www.google.com"]').css('color', 'green');
+
+$('*').hide();
